c3562c1abc271d3eacb506e141f82b753a14efc7
ykwei1127 <weieie1127@gmail.com>
2020-05-18 22:08:30 +0800

    add splitting test data code

--- data_split.py
+++ data_split.py
@@ -1,5 +1,7 @@
 import shutil
 import csv
+
+# train data split
 root = '/home/ykwei/Downloads/C1-P1_Train Dev_fixed/'
 fn = '/home/ykwei/Downloads/C1-P1_Train Dev_fixed/train.csv'
 with open(fn) as csvFile:
@@ -9,5 +11,16 @@ with open(fn) as csvFile:
 for row in data:
     src = root + 'C1-P1_Train/' + row[0]
     dest = root + 'train/' + row[1] + '/' + row[0]
+    shutil.copy(src, dest)
+
+# dev test data split
+fn2 = '/home/ykwei/Downloads/C1-P1_Train Dev_fixed/dev.csv'
+with open(fn2) as csvFile2:
+    csvReader = csv.reader(csvFile2)
+    data = list(csvReader)
+    data = data[1:]
+for row in data:
+    src = root + 'C1-P1_Dev/' + row[0]
+    dest = root + 'test/' + row[1] + '/' + row[0]
     shutil.copy(src, dest)
     
