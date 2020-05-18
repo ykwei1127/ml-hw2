f3b2611282fac25b1f470083a89878a1392a647b
ykwei1127 <weieie1127@gmail.com>
2020-05-18 17:45:38 +0800

    change data path to linux-241

--- data_split.py
+++ data_split.py
@@ -1,13 +1,13 @@
 import shutil
 import csv
-root = r'd:/機器學習/HW2/C1-P1_Train/'
-fn = r'd:/機器學習/HW2/test.csv'
+root = '/home/ykwei/Downloads/C1-P1_Train Dev_fixed/'
+fn = '/home/ykwei/Downloads/C1-P1_Train Dev_fixed/train.csv'
 with open(fn) as csvFile:
     csvReader = csv.reader(csvFile)
     data = list(csvReader)
     data = data[1:]
 for row in data:
-    src = root + row[0]
-    dest = root + row[1] + '\\' + row[0]
+    src = root + 'C1-P1_Train/' + row[0]
+    dest = root + 'train/' + row[1] + '/' + row[0]
     shutil.copy(src, dest)
     
